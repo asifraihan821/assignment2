56ae60a13ee9a65d106915537e9cd39663bcb007
asifraihan821 <asifraihan821@gmail.com>
2025-04-11 08:48:12 +0600

    Create student_database.py

---
+++ student_database.py
@@ -0,0 +1,14 @@
+class StudentDatabase:
+    student_list = []
+    
+    @classmethod
+    def add_student(cls,student):
+        cls.student_list.append(student)
+        
+        
+#st = StudentDatabase()
+#st.add_student('naima')
+#st.add_student('saima')
+#st.add_student('raima')
+
+#print(st.student_list)
