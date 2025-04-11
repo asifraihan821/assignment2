fb2b044e4469729a35104e228339c01f757701dc
asifraihan821 <asifraihan821@gmail.com>
2025-04-11 09:01:53 +0600

    Create question_2.py

---
+++ question_2.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self,studnt_id,name,department,is_enrolled):
+        self.studnt_id = studnt_id
+        self.name = name
+        self.department = department
+        self.is_enrolled = is_enrolled
+    
+    
+# st = Student(101,'asif','nothing',True)
+# print(st.stdnt_id)
+# print(Student(101,'asif','nothing',True).name)
